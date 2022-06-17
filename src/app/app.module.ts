624d7ed5a62efd6a069d56b06c31cad0d7d07f19
ServerJon <ctsesiphon@hotmail.com>
2022-06-18 00:14:22 +0200

    feat(01_i18n): custom i18n pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { I28nCurrencyPipe } from './pipes/i28n-currency.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    I28nCurrencyPipe
   ],
   imports: [
     BrowserModule,
